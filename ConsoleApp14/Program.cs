6511a1c3705feeadae69a67118e67aeedffe2639
nuyada <vova-isaev-2015@mail.ru>
2024-06-18 13:56:07 +0300

    Исправил чтоб работало со всеми словами

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -31,14 +31,15 @@ namespace ConsoleApp14
             if (modif.Length > 2)
             {
                 modif = modif.Remove(0, 2);
-                Console.WriteLine("..." + modif);
-                if (deep > 1)
-                {
-                    Echo(modif, deep - 1);
-                }
+                
+            }
+            Console.WriteLine("..." + modif);
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
             }
 
-           
+
         }
     }
 }
